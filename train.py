2f1d91ddd03ea11ff893c72be27b6f3edb496c44
mingyu cao <m.cao@surrey.ac.uk>
2026-02-22 19:03:48 +0000

    mod: model path

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ class TrainConfig:
     """Training hyperparameters for GRPO."""
     
     # --- Model ---
-    model_path: str = "/mnt/fast/nobackup/users/mc03002/models/LLaDA-8B-Instruct"
+    model_path: str = "/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct"
     
     # --- Training ---
     batch_size_per_device: int = 1
