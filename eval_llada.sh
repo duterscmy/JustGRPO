9537a23dddfc4c1fdff61f5f0250cc4fcb0e5077
mingyu cao <m.cao@surrey.ac.uk>
2026-03-01 16:15:05 +0000

    add

---
+++ eval_llada.sh
@@ -0,0 +1,9 @@
+
+model_path=$1
+mkdir -p eval_results
+torchrun --standalone --nproc-per-node=8 eval.py \
+  --ckpt_path $model_path \
+  --steps 256 \
+  --gen_length 256 \
+  --block_length 32
+# &> eval_results/$(basename $model_path).log
