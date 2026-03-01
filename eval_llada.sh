6359cb03924803c5eab7011dd7e317e2abf86600
mingyu cao <m.cao@surrey.ac.uk>
2026-03-01 16:27:50 +0000

    add

--- eval_llada.sh
+++ eval_llada.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name="ttrl"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --gres=gpu:2                # 请求8块GPU
+#SBATCH --time=3:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+
+
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate ttrl
 
 model_path=$1
 mkdir -p eval_results
-torchrun --standalone --nproc-per-node=3 eval.py \
+torchrun --standalone --nproc-per-node=2 eval.py \
   --ckpt_path $model_path \
   --steps 256 \
   --gen_length 256 \
-  --block_length 32
-# &> eval_results/$(basename $model_path).log
+  --block_length 32 &> eval_results/$(basename $model_path).log
