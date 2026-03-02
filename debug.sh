ff10ffbfdeb722bafcf8c2cd8a6fbd761146cbb3
mingyu cao <m.cao@surrey.ac.uk>
2026-03-02 16:36:35 +0000

    add

--- debug.sh
+++ debug.sh
@@ -1,4 +1,5 @@
 
 accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train.py \
   --run_dir ./checkpoints \
-  --grad_accum 16
+  --grad_accum 16 \
+  --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints/training-state-000014
