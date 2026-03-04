2351aa7a56258bbae2e1414d066d6509f237b6cb
mingyu cao <m.cao@surrey.ac.uk>
2026-03-04 22:01:25 +0000

    add

--- run_train_math500.sh
+++ run_train_math500.sh
@@ -1,6 +1,6 @@
 
 output_dir=./checkpoints_math500_num_generation8
 mkdir -p $output_dir
-accelerate launch --num_processes 2 --main_process_ip localhost --config_file configs/fsdp.yaml train_math500.py \
+accelerate launch --num_processes 1 --main_process_ip localhost --config_file configs/fsdp.yaml train_math500.py \
   --run_dir $output_dir \
   --grad_accum 1 #>> $output_dir.log 2>&1
