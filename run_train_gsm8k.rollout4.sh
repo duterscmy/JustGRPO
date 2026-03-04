91b121639259374fe5108821bda73f5b4eac372a
mingyu cao <m.cao@surrey.ac.uk>
2026-03-04 23:14:23 +0000

    add

---
+++ run_train_gsm8k.rollout4.sh
@@ -0,0 +1,9 @@
+
+output_dir=./checkpoints_gsm8k_num_generation4
+mkdir -p $output_dir
+
+#--resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints/training-state-000028
+
+accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_gsm8k.rollout4.py \
+  --run_dir $output_dir \
+  --grad_accum 16 >> $output_dir.log 2>&1
