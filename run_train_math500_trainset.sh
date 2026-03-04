869aa0c1691bf585b6a5f28140834e67900da720
mingyu cao <m.cao@surrey.ac.uk>
2026-03-04 19:48:00 +0000

    add

--- run_train_math500_trainset.sh
+++ run_train_math500_trainset.sh
@@ -3,4 +3,4 @@ output_dir=./checkpoints_math500_num_generation8_trainset
 mkdir -p $output_dir
 accelerate launch --num_processes 1 --main_process_ip localhost --config_file configs/fsdp.yaml train_math500_trainset.py \
   --run_dir $output_dir \
-  --grad_accum 1 >> $output_dir.log 2>&1
+  --grad_accum 1 #>> $output_dir.log 2>&1
