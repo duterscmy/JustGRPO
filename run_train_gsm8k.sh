5c7a6273d569a055348ff4a980e0412e4071d959
mingyu cao <m.cao@surrey.ac.uk>
2026-03-05 14:48:15 +0000

    add

--- run_train_gsm8k.sh
+++ run_train_gsm8k.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name="train_gsm8k_16"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4                # 请求8块GPU
+#SBATCH --time=17:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate ttrl
 
 output_dir=./checkpoints_gsm8k_num_generation16
 mkdir -p $output_dir
