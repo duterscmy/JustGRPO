2a96f5cd58d8894a97a281fc9ffa13f1f686b390
mingyu cao <m.cao@surrey.ac.uk>
2026-03-05 19:21:39 +0000

    add

--- run_train_math500.sh
+++ run_train_math500.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name="train_math_16"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4                # 请求8块GPU
+#SBATCH --time=24:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
 
-output_dir=./checkpoints_math500_num_generation8
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate ttrl
+
+
+output_dir=./checkpoints_math500_num_generation16
 mkdir -p $output_dir
-accelerate launch --num_processes 1 --main_process_ip localhost --config_file configs/fsdp.yaml train_math500.py \
+accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_math500.py \
   --run_dir $output_dir \
-  --grad_accum 1 #>> $output_dir.log 2>&1
+  --grad_accum 16 >> $output_dir.log 2>&1
