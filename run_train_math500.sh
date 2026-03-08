b468d20a46f0116c9b69429e4e085a90df176ed3
mingyu cao <m.cao@surrey.ac.uk>
2026-03-08 13:30:08 +0000

    add

--- run_train_math500.sh
+++ run_train_math500.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="train_math_16"
+#SBATCH --job-name="train_math8"
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:4                # 请求8块GPU
@@ -11,10 +11,10 @@
 source ~/.bashrc # 你的环境名
 conda activate ttrl
 
-
-output_dir=./checkpoints_math500_num_generation8_t0.3
+t=0.3
+output_dir=./checkpoints_math500_num_generation8_t${t}
 mkdir -p $output_dir
-accelerate launch --num_processes 1 --main_process_ip localhost --config_file configs/fsdp.yaml train_math500.py \
+accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_math500.py \
   --run_dir $output_dir \
-  --temperature 0.3 \
-  --grad_accum 16 #>> $output_dir.log 2>&1
+  --temperature ${t} \
+  --grad_accum 16 >> $output_dir.log 2>&1
