47eedeecd1e1686b962a4eba1531033bcad853d5
mingyu cao <m.cao@surrey.ac.uk>
2026-03-21 08:11:29 +0800

    add

---
+++ run_train_math500.block1.rank.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name="train_math8"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4                # 请求4块GPU
+#SBATCH --time=24:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate ttrl
+
+t=0.6
+block=1
+lr=1e-6
+output_dir=./checkpoints_math500_num_generation8_block${block}_t${t}_lr${lr}_rank_16_8
+
+#   --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_math500_num_generation8_t0.3/training-state-000005 \
+
+mkdir -p $output_dir
+accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_math500.rank.py \
+  --run_dir $output_dir \
+  --temperature ${t} \
+  --lr $lr \
+  --block_size $block \
+  --grad_accum 16 >> $output_dir.log 2>&1
