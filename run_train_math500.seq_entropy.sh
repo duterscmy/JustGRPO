0b86ac48e17f0065be1b4afade7f584d8d8e3a54
mingyu cao <m.cao@surrey.ac.uk>
2026-04-13 12:43:00 +0100

    add

--- run_train_math500.seq_entropy.sh
+++ run_train_math500.seq_entropy.sh
@@ -13,7 +13,7 @@ conda activate ttrl
 
 t=0.6
 block=1
-lr=1e-6
+lr=1e-7
 output_dir=./checkpoints_math500_num_generation8_block${block}_t${t}_lr${lr}_seq_entropy_reinforce
 
 #   --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_math500_num_generation8_block1_t0.6_lr1e-6/training-state-000005 \
@@ -24,4 +24,4 @@ accelerate launch --num_processes 4 --main_process_ip localhost --config_file co
   --temperature ${t} \
   --lr $lr \
   --block_size $block \
-  --grad_accum 4 >> $output_dir.log 2>&1
+  --grad_accum 16 >> $output_dir.log 2>&1
