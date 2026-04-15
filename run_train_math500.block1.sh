93ac0b5a5fc98777ba520348c03e492d628e4ee6
mingyu cao <m.cao@surrey.ac.uk>
2026-04-15 07:53:20 +0100

    add

--- run_train_math500.block1.sh
+++ run_train_math500.block1.sh
@@ -26,5 +26,5 @@ accelerate launch --num_processes 4 --main_process_ip localhost --config_file co
   --lr $lr \
   --block_size $block \
   --max_level $max_level \
-  --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_math500_num_generation8_block1_t0.6_lr1e-6_only_level1_3/training-state-000005 \
+  --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_math500_num_generation8_block1_t0.6_lr1e-6_only_level1_3/training-state-000010 \
   --grad_accum 8 >> $output_dir.log 2>&1
