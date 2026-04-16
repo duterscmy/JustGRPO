53d9ed8e986f7f389b18d25c9266caac2f0367f2
mingyu cao <m.cao@surrey.ac.uk>
2026-04-16 13:17:59 +0100

    add

--- sbatch_eval_mmlu.baseline.sh
+++ sbatch_eval_mmlu.baseline.sh
@@ -22,5 +22,5 @@ accelerate launch --num_processes 4 eval_llada.py \
   --tasks mmlu_flan_cot_zeroshot \
   --model llada_dist \
   --num_fewshot 0 \
-  --output_path eval_results/baseline/mmlu-${length}-${block} --log_samples \
-  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',enable_early_exit=false,enable_soar=false,gen_length=${length},steps=${length},block_length=${block},answer_length=5,torch_dtype=torch.bfloat16 &> logs/baseline-mmlu-ns0-length${length}-block${block}.log
+  --output_path eval_results/baseline/mmlu--${length}-${block}-${temperature} --log_samples \
+  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',enable_early_exit=false,enable_soar=false,gen_length=${length},steps=${length},block_length=${block},answer_length=5,torch_dtype=torch.bfloat16 &> logs/baseline-mmlu-ns0-length${length}-block${block}-temperature${temperature}.log
