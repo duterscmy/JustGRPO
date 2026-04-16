6596bc710dd4bf786dc4e5dae16473d7ba8646fc
mingyu cao <m.cao@surrey.ac.uk>
2026-04-16 17:38:19 +0100

    add

--- sbatch_eval_arc.baseline.sh
+++ sbatch_eval_arc.baseline.sh
@@ -10,6 +10,11 @@
 ### 激活conda环境
 source ~/.bashrc # 你的环境名
 conda activate soar
+python -c "import lm_eval; print(lm_eval.__file__)" &>> logs/debug.log
+cd /lus/lfs1aip2/projects/public/u6er/mingyu/lm-evaluation-harness
+pip install -e . --quiet
+python -c "import lm_eval; print(lm_eval.__file__)" &>> logs/debug.log
+cd /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO
 
 export HF_ENDPOINT=https://hf-mirror.com
 export HF_DATASETS_OFFLINE=0
@@ -18,9 +23,9 @@ export CUDA_VISIBLE_DEVICES=0
 length=256
 block=32
 temperature=0.0
-accelerate launch --num_processes 2 eval_llada.py \
+accelerate launch --num_processes 1 eval_llada.py \
   --tasks arc_cot_zeroshot \
   --model llada_dist \
   --num_fewshot 0 \
   --output_path eval_results/baseline/arc-${length}-${block}-${temperature} --log_samples \
-  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',temperature=${temperature},enable_early_exit=false,enable_soar=false,gen_length=${length},steps=${length},block_length=${block},answer_length=5,torch_dtype=torch.bfloat16 &> logs/baseline-arc-ns0-length${length}-block${block}-temperature${temperature}.log
+  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',temperature=${temperature},enable_early_exit=false,enable_soar=false,gen_length=${length},steps=${length},block_length=${block},answer_length=5,torch_dtype=torch.bfloat16 #&> logs/baseline-arc-ns0-length${length}-block${block}-temperature${temperature}.log
