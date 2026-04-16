db0200d3b505c28b07ab0256247097d45bcdc149
mingyu cao <m.cao@surrey.ac.uk>
2026-04-16 13:06:19 +0100

    add

---
+++ sbatch_eval_bbh.baseline.sh
@@ -0,0 +1,25 @@
+ß#!/bin/bash
+#SBATCH --job-name="soar_eval"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1                # 请求2块GPU
+#SBATCH --time=24:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate soar
+
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_DATASETS_OFFLINE=0
+export CUDA_VISIBLE_DEVICES=0
+#  --limit 256
+length=256
+block=32
+accelerate launch eval_llada.py \
+  --tasks bbh_cot_zeroshot \
+  --model llada_dist \
+  --num_fewshot 0 \
+  --output_path eval_results/baseline/bbh-${length}-${block} --log_samples \
+  --model_args model_path='/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct',enable_early_exit=false,enable_soar=false,gen_length=${length},steps=${length},block_length=${block},answer_length=5,torch_dtype=torch.bfloat16 &> logs/baseline-bbh-ns0-length${length}-block${block}.log
