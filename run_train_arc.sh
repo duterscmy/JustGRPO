bc92114ee8f73c9ed1657a4ba0c3703bd0561671
mingyu cao <m.cao@surrey.ac.uk>
2026-04-17 07:06:49 +0100

    add

--- run_train_arc.sh
+++ run_train_arc.sh
@@ -16,8 +16,8 @@ mkdir -p $output_dir
 
 #--resume_ckpt 
 
-accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_arc.py \
+accelerate launch --num_processes 1 --main_process_ip localhost --config_file configs/fsdp.yaml train_arc.py \
   --run_dir $output_dir \
   --block_size 32 \
-  --grad_accum 8 \
-  --temperature 1.0 >> $output_dir.log 2>&1
+  --grad_accum 1 \
+  --temperature 1.0 #>> $output_dir.log 2>&1
