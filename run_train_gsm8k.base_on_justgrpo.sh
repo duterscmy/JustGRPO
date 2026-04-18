24a202a4554ae7d76b1f4bcd5fe917ef3fbe3d33
mingyu cao <m.cao@surrey.ac.uk>
2026-04-18 20:38:46 +0100

    add

---
+++ run_train_gsm8k.base_on_justgrpo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name="train_gsm8k_8"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4                # 请求8块GPU
+#SBATCH --time=24:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate ttrl
+
+block=32
+t=1.0
+lr=1e-6
+output_dir=./checkpoints_gsm8k_num_generation8_test_block${block}_temperature${t}_lr${lr}_base_on_justgrpo_0418
+mkdir -p $output_dir
+
+#--resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints/training-state-000028
+
+accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_gsm8k.rollout8.py \
+  --run_dir $output_dir \
+  --model_path /lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-Instruct-JustGRPO-GSM8K \
+  --block_size 32 \
+  --lr 1e-6 \
+  --temperature 1.0 \
+  --total_steps 5 --save_every 5 \
+  --grad_accum 16 >> $output_dir.log 2>&1
