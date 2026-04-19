0ae9c489c0d6e5e75cf36b929d6e4967b8c1fb88
mingyu cao <m.cao@surrey.ac.uk>
2026-04-19 23:21:52 +0100

    add

--- run_train_gsm8k.rollout8.block32.sh
+++ run_train_gsm8k.rollout8.block32.sh
@@ -24,6 +24,6 @@ accelerate launch --num_processes 4 --main_process_ip localhost --config_file co
   --block_size 32 \
   --lr $lr  \
   --temperature 1.0 \
-  --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_gsm8k_num_generation8_test_block32_temperature1.0_lr5e-6_0418/training-state-000010 \
-  --total_steps 20 --save_every 5 \
+  --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_gsm8k_num_generation8_test_block32_temperature1.0_lr5e-6_0418/training-state-000020 \
+  --total_steps 30 --save_every 5 \
   --grad_accum 16 >> $output_dir.log 2>&1
