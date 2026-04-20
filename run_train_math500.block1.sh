d92b415e5531ca90df77b463892c5b703af5e8dc
mingyu cao <m.cao@surrey.ac.uk>
2026-04-20 20:57:02 +0100

    add

--- run_train_math500.block1.sh
+++ run_train_math500.block1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name="train_math8"
+#SBATCH --job-name="train_math"
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
-#SBATCH --gres=gpu:4                # 请求8块GPU
+#SBATCH --gres=gpu:4
 #SBATCH --time=24:00:00
 #SBATCH -o slurm.%j.%N.out
 #SBATCH -e slurm.%j.%N.err
@@ -13,9 +13,9 @@ conda activate ttrl
 
 t=0.6
 block=1
-lr=1e-6
+lr=5e-6
 max_level=5
-output_dir=./checkpoints_math500_num_generation8_block${block}_t${t}_lr${lr}_only_level1_${max_level}
+output_dir=./checkpoints_math500_num_generation8_block${block}_t${t}_lr${lr}_level1_${max_level}_0419_batch64
 
 #   --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_math500_num_generation8_block1_t0.6_lr1e-6/training-state-000005 \
 
@@ -26,4 +26,5 @@ accelerate launch --num_processes 4 --main_process_ip localhost --config_file co
   --lr $lr \
   --block_size $block \
   --max_level $max_level \
-  --grad_accum 8 >> $output_dir.log 2>&1
+  --total_steps 10 --save_every 5 \
+  --grad_accum 16 >> $output_dir.log 2>&1
