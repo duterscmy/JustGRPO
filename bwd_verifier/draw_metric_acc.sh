ecc93bb410ceea04d64cc60e3ec33a949d378a47
mingyu cao <m.cao@surrey.ac.uk>
2026-04-21 20:34:16 +0100

    add

--- bwd_verifier/draw_metric_acc.sh
+++ bwd_verifier/draw_metric_acc.sh
@@ -1,6 +1,6 @@
 input=$1
 base=$(basename "$input" .json)
-python plot_score_vs_accuracy.py "$input" \
+python draw_metric_acc.py "$input" \
     --metrics confidence backward_digits backward_probability \
     --n_buckets 10 \
     --save_dir "${base}_fig" \
