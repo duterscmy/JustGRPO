9da13c598cdaa9916b4ccdacc5b3906b41bc13a1
mingyu cao <m.cao@surrey.ac.uk>
2026-04-22 19:57:50 +0100

    add

---
+++ run_train_math500.block32.base_on_justgrpo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name="train_math"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4
+#SBATCH --time=24:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate ttrl
+
+t=0.6
+block=32
+lr=5e-6
+max_level=3
+output_dir=./checkpoints_math500_num_generation16_block${block}_t${t}_lr${lr}_level1_${max_level}_0422_batch32_based_on_justgrpo
+
+#   --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_math500_num_generation8_block1_t0.6_lr1e-6/training-state-000005 \
+
+mkdir -p $output_dir
+accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_math500.py \
+  --model_path /lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-Instruct-JustGRPO-Math500 \
+  --run_dir $output_dir \
+  --temperature ${t} \
+  --lr $lr \
+  --block_size $block \
+  --max_level $max_level \
+  --total_steps 10 --save_every 2 \
+  --grad_accum 8 >> $output_dir.log 2>&1
