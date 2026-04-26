d562f626e2ce3e2df0510af8dfce811b10485574
mingyu cao <m.cao@surrey.ac.uk>
2026-04-26 10:09:20 +0100

    add

--- run_train_arc.block32.sh
+++ run_train_arc.block32.sh
@@ -14,13 +14,13 @@ conda activate ttrl
 block=32
 t=1.0
 lr=1e-6
-length=64
+length=128
 output_dir=./checkpoints_arc_num_generation8_test_length${length}_block${block}_t${t}_lr${lr}_0425_batch64
 mkdir -p $output_dir
 
 #--resume_ckpt 
 accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_arc.py \
-  --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_arc_num_generation8_test_block32_t1.0_lr1e-6_0418_batch64/training-state-000030 \
+  --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_arc_num_generation8_test_length128_block32_t1.0_lr1e-6_0425_batch64/training-state-000030 \
   --run_dir $output_dir \
   --block_size $block \
   --grad_accum 16 \
