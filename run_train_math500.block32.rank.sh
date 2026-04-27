303c150e216ea0a8e12cfa99b6343fd4f65ce7c5
mingyu cao <m.cao@surrey.ac.uk>
2026-04-27 16:48:36 +0100

    add

--- run_train_math500.block32.rank.sh
+++ run_train_math500.block32.rank.sh
@@ -14,8 +14,8 @@ conda activate ttrl
 t=0.6
 block=32
 lr=5e-6
-max_level=2
-output_dir=./checkpoints_math500_num_generation8_block${block}_t${t}_lr${lr}_level1_${max_level}_0426_batch32_weighted_confidence
+max_level=5
+output_dir=./checkpoints_math500_num_generation8_block${block}_t${t}_lr${lr}_level1_${max_level}_0427_batch32_weighted_confidence
 
 #   --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_math500_num_generation8_block1_t0.6_lr1e-6/training-state-000005 \
 
@@ -27,4 +27,4 @@ accelerate launch --num_processes 4 --main_process_ip localhost --config_file co
   --block_size $block \
   --max_level $max_level \
   --total_steps 10 --save_every 2 \
-  --grad_accum 8 >> $output_dir.log 2>&1
+  --grad_accum 16 >> $output_dir.log 2>&1
