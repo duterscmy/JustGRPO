9f1d3421e66bc3faef71417a03e772d31682ba56
mingyu cao <m.cao@surrey.ac.uk>
2026-04-29 08:47:57 +0100

    add

--- bwd_verifier/rollout_gsm8k.sh
+++ bwd_verifier/rollout_gsm8k.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=llada_gsm8k_rollout
 #SBATCH --output=logs/llada_gsm8k_rollout_%j.out
 #SBATCH --error=logs/llada_gsm8k_rollout_%j.err
-#SBATCH --time=72:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
@@ -21,16 +21,18 @@ mkdir -p logs
 export CUDA_VISIBLE_DEVICES=0
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-seed=1
+seed=42
+block=$1
+t=$2
 # Run the evaluation script with default parameters
 python rollout_gsm8k.py \
     --steps 256 \
     --gen_length 256 \
-    --block_length 32 \
-    --temperature 1.0 \
+    --block_length ${block} \
+    --temperature ${t} \
     --remasking low_confidence \
     --num_rollouts 8 \
-    --max_problems -1 \
+    --max_problems 300 \
     --output_file gsm8k_results.add_records.tmp1.0.v${seed}.json \
     --verbose \
     --model_path /mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct \
