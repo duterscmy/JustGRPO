187cb4677e2ee998aab6d1aacb97646fe78d53c6
mingyu cao <m.cao@surrey.ac.uk>
2026-04-29 20:17:47 +0100

    add

---
+++ run_train_arc.justgrpo.qiao.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name="train_arc"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4
+#SBATCH --time=24:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+#SBATCH --cpus-per-task=18
+#SBATCH --mem 64G
+#SBATCH --partition=gpu_h100
+
+# Create logs directory if it doesn't exist
+mkdir -p logs
+source /gpfs/home5/xiaoq/.bashrc
+conda activate ttrl
+cd /home/xiaoq/caomingyu/justgrpo
+
+block=32
+t=1.0
+lr=1e-6
+length=128
+output_dir=./checkpoints_arc_num_generation8_test_length${length}_block${block}_t${t}_lr${lr}_0429_batch64_justgrpo
+mkdir -p $output_dir
+
+#--resume_ckpt 
+accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_arc.justgrpo.py \
+  --model_path /gpfs/home5/xiaoq/caomingyu/models/LLaDA-8B-Instruct \
+  --run_dir $output_dir \
+  --block_size $block \
+  --grad_accum 16 \
+  --lr $lr \
+  --total_steps 30 --save_every 5 \
+  --temperature $t \
+  --gen_length $length \
+  --gen_steps $length >> $output_dir.log 2>&1
