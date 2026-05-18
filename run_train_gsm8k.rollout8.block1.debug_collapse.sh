d2feeffcbbef86795a477af01b69391163f99e79
mingyu cao <m.cao@surrey.ac.uk>
2026-05-18 21:02:43 +0100

    add

--- run_train_gsm8k.rollout8.block1.debug_collapse.sh
+++ run_train_gsm8k.rollout8.block1.debug_collapse.sh
@@ -20,11 +20,11 @@ mkdir -p $output_dir
 #--resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints/training-state-000028
 
 accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_gsm8k.rollout8.debug_collapse.py \
-  --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_gsm8k_num_generation8_test_block1_temperature1.0_lr5e-6_0513_debug_collapse/training-state-000020 \
+  --resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints_gsm8k_num_generation8_test_block1_temperature1.0_lr5e-6_0513_debug_collapse/training-state-000040 \
   --run_dir $output_dir \
   --block_size $block \
   --lr $lr  \
   --temperature $t \
-  --total_steps 40 --save_every 5 \
+  --total_steps 60 --save_every 5 \
   --policy_shift_stride 8 \
   --grad_accum 8 >> $output_dir.log 2>&1
