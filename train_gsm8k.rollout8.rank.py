9f2a73faa1d02e017c4138fdeb82fa8e34279830
mingyu cao <m.cao@surrey.ac.uk>
2026-07-30 08:58:13 +0800

    add

--- train_gsm8k.rollout8.rank.py
+++ train_gsm8k.rollout8.rank.py
@@ -16,7 +16,7 @@ class TrainConfig:
     """Training hyperparameters for GRPO."""
     
     # --- Model ---
-    model_path: str = "/lus/lfs1aip2/projects/public/u6er/mingyu/models/LLaDA-8B-Instruct"
+    model_path: str = "/lus/lfs1aip2/projects/public/u6nc/mingyu/models/LLaDA-8B-Instruct"
     
     # --- Training ---
     batch_size_per_device: int = 1
