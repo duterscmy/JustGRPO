9b8c0afe08713b4efc9cb95627aa38b3ec5b7a5b
mingyu cao <m.cao@surrey.ac.uk>
2026-08-10 14:58:29 +0800

    add

---
+++ run_train_gsm8k.rollout8.block1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name="train_gsm8k"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:4
+#SBATCH --time=24:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate ttrl
+
+block=1
+t=1.0
+lr=5e-6
+output_dir=./checkpoints_gsm8k_num_generation8_test_block${block}_temperature${t}_lr${lr}_0810
+mkdir -p $output_dir
+
+#--resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints/training-state-000028
+
+accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_gsm8k.rollout8.py \
+  --run_dir $output_dir \
+  --block_size $block \
+  --lr $lr  \
+  --temperature $t \
+  --total_steps 30 --save_every 5 \
+  --grad_accum 8 >> $output_dir.log 2>&1
