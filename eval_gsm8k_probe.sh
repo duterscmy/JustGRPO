de8834cb20c53ace61a2afd58f3d8fc9a5ad1e47
mingyu cao <m.cao@surrey.ac.uk>
2026-08-17 17:30:49 +0800

    add

--- eval_gsm8k_probe.sh
+++ eval_gsm8k_probe.sh
@@ -20,20 +20,20 @@ mkdir -p probe_results
 clean_path=$(echo $model_path | sed 's:/*$::')
 parent_dir=$(basename $(dirname "$clean_path"))
 base_name=$(basename "$clean_path")
-target_dir="probe_results/${parent_dir}"
+target_dir="probe_results/${parent_dir}_${length}_${block}"
 mkdir -p "$target_dir"
-log_path="${target_dir}/${base_name}.gsm8k.${length}.${block}.log"
+log_path="${target_dir}/eval.log"
 
 torchrun --standalone --nproc_per_node=4 \
   eval_gsm8k_probe.py \
   --model_path $model_path \
   --tokenizer_path /lus/lfs1aip2/projects/public/u6os/mingyu/models/LLaDA-8B-Instruct \
   --output_dir $target_dir \
-  --block_size 32 \
+  --block_size $block  \
   --num_questions 100 \
   --num_rollouts 64 \
   --rollout_batch_size 4 \
   --temperature 1.0 \
-  --steps 256 \
-  --gen_length 256 \
+  --steps $length \
+  --gen_length $length \
   --k_values 1 2 4 8 16 32 64 &> "$log_path"
