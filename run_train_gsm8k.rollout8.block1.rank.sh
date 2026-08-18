d074d82078faa612a43f56ece0f405002ef73bcc
mingyu cao <m.cao@surrey.ac.uk>
2026-08-18 10:44:48 +0800

    add

--- run_train_gsm8k.rollout8.block1.rank.sh
+++ run_train_gsm8k.rollout8.block1.rank.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="train_gsm8k"
+#SBATCH --job-name="train_gsm8k_block1"
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:4
@@ -20,10 +20,10 @@ mkdir -p $output_dir
 #--resume_ckpt /lus/lfs1aip2/projects/public/u6er/mingyu/justGRPO/checkpoints/training-state-000028
 
 accelerate launch --num_processes 4 --main_process_ip localhost --config_file configs/fsdp.yaml train_gsm8k.rollout8.debug_collapse.py \
-  --model_path /lus/lfs1aip2/projects/public/u6os/mingyu/justgrpo/checkpoints_gsm8k_num_generation8_test_block1_temperature1.0_lr5e-6_0811_rank/ckpt-000045 \
+  --resume_ckpt /lus/lfs1aip2/projects/public/u6os/mingyu/justgrpo/checkpoints_gsm8k_num_generation8_test_block1_temperature1.0_lr5e-6_0811_rank_continue/training-state-000020 \
   --run_dir $output_dir \
   --block_size $block \
   --lr $lr  \
   --temperature $t \
-  --total_steps 20 --save_every 5 \
+  --total_steps 40 --save_every 5 \
   --grad_accum 8 >> $output_dir.log 2>&1
