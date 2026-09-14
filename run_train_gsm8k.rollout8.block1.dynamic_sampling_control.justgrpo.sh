2fc65812d2cd675507e97f9366edcc42ad9e19b1
mingyu cao <m.cao@surrey.ac.uk>
2026-09-14 22:03:33 +0100

    add

--- run_train_gsm8k.rollout8.block1.dynamic_sampling_control.justgrpo.sh
+++ run_train_gsm8k.rollout8.block1.dynamic_sampling_control.justgrpo.sh
@@ -46,7 +46,7 @@ accelerate launch \
   --lr "$lr" \
   --temperature "$initial_temperature" \
   --total_steps 4 \
-  --save_every 2 \
+  --save_every 1 \
   --grad_accum "$grad_accum" \
   --scale_by_grad_accum \
   --dynamic_sampling \
